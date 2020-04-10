abeb55bafa6331af26da4b1f40be33b450c7eafc
TommasoPeletta <tommaso.peletta@etu.unige.ch>
2020-04-10 13:34:26 +0200

    first gun animation

---
+++ Aimotion/Assets/Scripts/GunFire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+		AudioSource gunsound = GetComponent<AudioSource>();
+		gunsound.Play();
+		Animation gunshot = GetComponent<Animation>();
+		gunshot.Play("GunShot");
+	}
+
+    }
+}
