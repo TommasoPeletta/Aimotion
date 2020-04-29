e7013a4fb2be93727655492da94d8cd83c0a8645
Jonathan Lo <jonathan.lo@etu.unige.ch>
2020-04-29 14:50:58 +0200

    dynamic music and background

---
+++ Aimotion/Assets/Scripts/audio_behaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio_behaviour : MonoBehaviour
+{
+
+	public AudioSource _AudioSource1;
+	public AudioSource _AudioSource2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	_AudioSource1.Play();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)){
+		if (_AudioSource1.isPlaying){
+			_AudioSource1.Stop();
+			_AudioSource2.Play();
+		}else{
+			_AudioSource2.Stop();
+			_AudioSource1.Play();
+		}
+	}
+    }
+}
---
+++ Aimotion/Assets/Scripts/sky_behaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sky_behaviour : MonoBehaviour
+{
+/*
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+	public int level = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+	//if (level == 0)
+	if (Input.GetKeyDown(KeyCode.M))
+		if (RenderSettings.skybox.HasProperty("_Tint"))
+			RenderSettings.skybox.SetColor("_Tint",Color.blue);
+		else if (RenderSettings.skybox.HasProperty("_SkyTint"))
+			RenderSettings.skybox.SetColor("_SkyTint",Color.blue);
+	//else if (level == 1)
+	else if (Input.GetKeyDown(KeyCode.M))
+			RenderSettings.skybox.SetColor("_Tint",Color.red);
+		else if (RenderSettings.skybox.HasProperty("_SkyTint"))
+			RenderSettings.skybox.SetColor("_SkyTint",Color.red);
+    }
+*/
+
+//public static void UpdateEnvironment();
+
+
+
+
+
+public Material[] skyboxes;
+
+// Use this for initialization
+ void Start () {
+    ChangeMySkybox();
+ }
+ 
+ // Update is called once per frame
+ void Update () {
+	if (Input.GetKeyDown(KeyCode.S))
+		ChangeMySkybox();
+ }
+
+void ChangeMySkybox()
+ {
+    int x = Random.Range(0, skyboxes.Length);
+    RenderSettings.skybox = skyboxes[x];
+	DynamicGI.UpdateEnvironment ();
+ }
+
+
+
+
+
+}
